512e69108007103b3489cfeca74380f2a8720613
Hyun Kim <justinkimkorea@gmail.com>
2023-03-15 21:32:53 +1200

    utilise `system` role

--- src/util/summarise.ts
+++ src/util/summarise.ts
@@ -1,25 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
 
 export async function summarise(text: string, { apiKey }: { apiKey: string }) {
   try {
     const response = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
+      'https://api.openai.com/v1/chat/completions',
       {
-        model: "gpt-3.5-turbo",
+        model: 'gpt-3.5-turbo',
         messages: [
-          { role: "user", content: `outline the transcription: ${text}` },
+          {
+            role: 'system',
+            content: `You are a helpful assistant who outlines, in point format, important details of text provided to you`,
+          },
+          { role: 'user', content: `${text}` },
         ],
       },
       {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${apiKey}`,
         },
-      }
+      },
     );
     console.log(response.data);
     return response.data.choices[0].message.content;
   } catch (e) {
-    console.error("Error summarising:", e);
+    console.error('Error summarising:', e);
   }
 }
